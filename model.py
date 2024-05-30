e036a16ffbd2197853ad561157901452cbf0d90d
Hussam <o.hussam@gmail.com>
2024-05-30 13:13:09 +0300

    almost ch1

--- model.py
+++ model.py
@@ -3,6 +3,18 @@ from dataclasses import dataclass
 from datetime import date
 from typing import Optional, List, Set
 
+class OutOfStock(Exception):
+    pass
+
+def allocate(line: OrderLine, batches: list[Batch]) -> str:
+    try:
+        batch = next(b for b in sorted(batches) if b.can_allocate(line))
+        batch.allocate(line)
+        return batch.referance
+    except StopIteration:
+        raise OutOfStock(f"Out of stock for sku {line.sku}")
+
+
 @dataclass(frozen=True)
 class OrderLine:
     orderid: str
@@ -10,17 +22,48 @@ class OrderLine:
     qty: str
 
 class Batch:
+
     def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]) -> None:
-        self.ref = ref
+        self.referance = ref
         self.sku = sku
         self._purchased_quantity = qty
         self.eta = eta
         self._allocations = set()
     
-    def allocate(self, line: OrderLine) -> None:
-        self.available_quantity -= line.qty
+    def __hash__(self) -> int:
+        return hash(self.referance)
     
+    def __repr__(self) -> str:
+        return f"<Batch {self.referance}>"
+    
+    def __eq__(self, other: Batch) -> bool:
+        if not isinstance(other, Batch):
+            return False
+        return self.referance == other.referance
+
+    def __gt__(self, other: Batch):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta 
+    
+    def deallocate(self, line: OrderLine) -> None:
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    def allocate(self, line: OrderLine) -> None:
+        if self.can_allocate(line):
+            self._allocations.add(line)
+    
+
     @property
     def allcoated_quantity(self) -> int:
         return sum(line.qty for line in self._allocations)
     
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allcoated_quantity
+    
+    def can_allocate(self, line: OrderLine) -> bool:
+        return line.sku == self.sku and self.available_quantity >= line.qty
--- test_batches.py
+++ test_batches.py
@@ -1,7 +1,30 @@
 from model import Batch, OrderLine
 from datetime import date
+import pytest
+
+def make_batch_and_line(sku, batch_qty, line_qty):
+    return (
+        Batch("batch001", sku, batch_qty, eta=date.today()),
+        OrderLine("order-123", sku, line_qty),
+    )
+
 def test_allocating_to_a_batch_reduce_available_quantity():
     batch = Batch("batch-001", "SMALL_TABLE", qty=20, eta=date.today())
     line = OrderLine("order-ref", "SMALL_TABLE", 2)
     batch.allocate(line)
     assert batch.available_quantity == 18
+
+@pytest.mark.parametrize(['batch_qty', 'line_qty', 'expected'],[
+    (20, 2, True), # batch.qty > line.qty
+    (2, 20, False),  # batch.qty < line.qty
+    (2, 2, True),  # batch.qty == line.qty
+
+])
+def test_can_allocate_(batch_qty, line_qty, expected):
+    batch, line = make_batch_and_line("BLUE_TABLE", batch_qty, line_qty)
+    assert batch.can_allocate(line) is expected
+
+def test_can_allocate_if_skus_do_not_match():
+    batch = Batch("batch001", "BLUE_SMALL_TABLE", 100, eta=date.today())
+    line = OrderLine("order-123", "SMALL_TABLE", 10)
+    assert batch.can_allocate(line) is False
